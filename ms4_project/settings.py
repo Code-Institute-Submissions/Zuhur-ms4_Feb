8ff818bd60f9b3339f08bf8d9c40916cfebc9df8
Zuhur <zuhur.h.94@gmail.com>
2021-12-17 23:06:13 +0100

    Create membership app, template and view for membership page

--- ms4_project/settings.py
+++ ms4_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'gym_app',
     'merchandise',
+    'membership',
 ]
 
 MIDDLEWARE = [
--- ms4_project/urls.py
+++ ms4_project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('gym_app.urls')),
     path('merch/', include('merchandise.urls')),
+    path('membership/', include('membership.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
