d4d61cd793da4a3a76fd38ea9e211d90521d01a7
Zuhur <zuhur.h.94@gmail.com>
2021-12-20 16:57:09 +0100

    Add checkout template content, views

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render, reverse
+from .forms import OrderForm
+from django.contrib import messages
 # Create your views here.
+
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "Theres nothing in your bag")
+        return redirect(reverse('merch'))
+
+    order_form = OrderForm()
+    context = {
+        'order_form': order_form,
+    }
+    return render(request, 'checkout/checkout.html', context)
--- ms4_project/urls.py
+++ ms4_project/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('merch/', include('merchandise.urls')),
     path('membership/', include('membership.urls')),
     path('bag/', include('bag.urls')),
+    path('checkout/', include('checkout.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
