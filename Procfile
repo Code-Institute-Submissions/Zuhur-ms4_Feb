848d278b1c692eac6610a77ab6a467c800a4905c
Zuhur <zuhur.h.94@gmail.com>
2021-12-20 23:54:38 +0100

    Begin deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ms4_project.wsgi:application
--- ms4_project/settings.py
+++ ms4_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 
 if os.path.exists("env.py"):
@@ -28,7 +29,7 @@ SECRET_KEY = 'django-insecure-48pi42*0)(8v*xxfo6z5b*1o=bqmvv9ul9se&2s@*k9+h=a(sn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['milestone-four.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -120,12 +121,19 @@ WSGI_APPLICATION = 'ms4_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+
 
 
 # Password validation
