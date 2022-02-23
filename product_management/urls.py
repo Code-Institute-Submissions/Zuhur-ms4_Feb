8b9051b64ec7e86b60914a05a10f44bd65ce26db
Zuhur Hassan <salmahassan@Salmas-MacBook-Air.local>
2022-02-23 09:32:27 +0100

    Add page for managing products

--- product_management/urls.py
+++ product_management/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.product_management, name='product-management'),
+    path('add_product/', views.add_product, name='add-product'),
 ]
--- product_management/views.py
+++ product_management/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import redirect, render, HttpResponse
 
 def product_management(request):
     return render(request, 'product_management/product_management.html')
+
+def add_product(request):
+    return render(request, 'product_management/add_product.html')
