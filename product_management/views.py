e23cd8ef5ffca5b52468b85d580241fea6518daa
Zuhur Hassan <salmahassan@Salmas-MacBook-Air.local>
2022-02-24 01:45:55 +0100

    Add authentication for users who can access product management rights

--- product_management/views.py
+++ product_management/views.py
@@ -1,16 +1,22 @@
 from audioop import reverse
+import django
 from django.shortcuts import redirect, render, reverse
+from django.contrib.auth.decorators import login_required, user_passes_test
 from merchandise.models import Merchandise
-
 from merchandise.views import merchandise
 from .forms import ProductForm
 # Create your views here.
 
 
+@login_required
 def product_management(request):
-    return render(request, 'product_management/product_management.html')
+    if request.user.is_superuser:
+        return render(request, 'product_management/product_management.html')
+    else:
+        return redirect(reverse('merch'))
 
 
+@login_required
 def add_product(request):
     form = ProductForm()
 
@@ -23,9 +29,14 @@ def add_product(request):
     context = {
         'form': form
     }
-    return render(request, 'product_management/add_product.html', context)
+    if request.user.is_superuser:
+        return render(request, 'product_management/add_product.html', context)
+    else:
+        return redirect(reverse('merch'))
+    
 
 
+@login_required
 def edit_product(request, pk):
     merch = Merchandise.objects.get(id=pk)
     form = ProductForm(instance=merch)
@@ -38,9 +49,14 @@ def edit_product(request, pk):
     context = {
         'form': form
     }
-    return render(request, 'product_management/edit_product.html', context)
+    if request.user.is_superuser:
+        return render(request, 'product_management/edit_product.html', context)
+    else:
+        return redirect(reverse('merch'))
+    
 
 
+@login_required
 def delete_product(request, pk):
     merch = Merchandise.objects.get(id=pk)
 
@@ -51,4 +67,7 @@ def delete_product(request, pk):
     context = {
         'product': merch,
     }
-    return render(request, 'product_management/delete_product.html', context)
+    if request.user.is_superuser:
+        return render(request, 'product_management/edit_product.html', context)
+    else:
+        return redirect(reverse('merch'))
